513283d616029f860ec01a44ec48ca0edf4531bb
Faiber <126357878+Faiber8624@users.noreply.github.com>
2024-05-19 21:06:45 -0500

    Add files via upload

--- src/app/pages/loginPage/loginPage.js
+++ src/app/pages/loginPage/loginPage.js
@@ -20,33 +20,65 @@ function LoginPage() {
   };
 
   return (
-    <div className="container">
-      <h1>Bienvenido a login</h1>
+    <div className='container-padre'>
 
-      <h2>Ingresa tu nombre de usuario</h2>
-      <p>
-        todos los componentes crecen al 100% del espacio que tengan, entonces ya
-        depende de ti el tamaño
-      </p>
+      <div className='container-arbol'>
+        <img className='imagen-arbol' src="/assets/images/arbol.png" alt="Imagen arbol"/>
+        <img className='imagen-LogoGlamping' src="/assets/images/LogoG.svg" alt="Imagen logo"/>
+      </div>
+      
+      <div className="container">
+        <h1>LOGIN</h1>
 
-      <Input
-        type={'text'}
-        value={formValues.username}
-        onChange={(value) => handleInputChange('username', value)}
-        placeholder={'Nombre de usuario'}
-        className={''}
-      />
+        <Input
+          type={'text'}
+          value={formValues.username}
+          onChange={(value) => handleInputChange('username', value)}
+          placeholder={'Usurname'}
+          className={'usuario'}
+        />
 
-      <Button
-        text="Ver Formulario"
-        color="var(--text-color-bg)"
-        mode="outline"
-        textColor="var(--text-color-bg)"
-        onClick={() => {
-          handleClick();
-        }}
-      />
+        <Input
+          type={'text'}
+          value={formValues.Password}
+          onChange={(value) => handleInputChange('Password', value)}
+          placeholder={'Password'}
+          className={'input_password'}
+        />
+        <div className='div_check'>
+          <input className='input_check' type="checkbox" name="my-checkbox" id="opt-in" />
+          <label className='label_check' for="opt-in">Renember Password</label>
+        </div>
+
+        <div className='div_boton'>
+          <Button
+            className={'btn_login'}
+            text="LOGIN"
+            color="var(--secondary-color)"
+            mode="fill"
+            textColor="var(--text-color-bg)"
+            onClick={() => {
+              handleClick('login');
+            }}
+          />
+
+        </div>
+
+        <div>
+          <p className='P_text'>Forgot Password?</p>
+          <p className='P_text'>Don't have account?</p>
+        </div>
+
+
+
+
+      </div>      
+
     </div>
+    
+
+
+
   );
 }
 
--- src/app/pages/registerPage/registerPage.js
+++ src/app/pages/registerPage/registerPage.js
@@ -1,10 +1,124 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '../../components/general-button/button';
+import Input from '../../components/input/input';
+import './registerPage.scss';
 
 function RegisterPage() {
+  const [formValues, setFormValues] = useState({
+    username: '',
+  });
+
+  const handleInputChange = (name, value) => {
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  const handleClick = () => {
+    alert(`Se escribió: ${formValues.username}`);
+  };
+
   return (
-    <div>
-      <h1>Bienvenido al registro</h1>
+    <div className='container-padre'>
+
+      <div className='container-arbol'>
+        <img className='imagen-arbol' src="/assets/images/arbol.png" alt="Descripción de la imagen" />
+      </div>
+
+      <div className="container">
+        <h1>REGISTRARSE</h1>
+
+        <div className='container-inputpadre'>
+          <div className='container-input'>
+            <Input
+              type={'text'}
+              value={formValues.username}
+              onChange={(value) => handleInputChange('Full name', value)}
+              placeholder={'Full name'}
+              className={'Full-name'}
+            />
+
+            <Input
+              type={'text'}
+              value={formValues.username}
+              onChange={(value) => handleInputChange('contraseña', value)}
+              placeholder={'Address'}
+              className={'Address'}
+            />
+
+            <Input
+              type={'text'}
+              value={formValues.username}
+              onChange={(value) => handleInputChange('contraseña', value)}
+              placeholder={'Password'}
+              className={'input_password'}
+            />
+          </div>
+
+          <div className='container-input2'>
+            <Input
+              type={'text'}
+              value={formValues.username}
+              onChange={(value) => handleInputChange('contraseña', value)}
+              placeholder={'Email address'}
+              className={'Email-address'}
+            />
+
+            <Input
+              type={'text'}
+              value={formValues.username}
+              onChange={(value) => handleInputChange('contraseña', value)}
+              placeholder={'Phone No'}
+              className={'phone'}
+            />
+
+            <Input
+              type={'text'}
+              value={formValues.username}
+              onChange={(value) => handleInputChange('contraseña', value)}
+              placeholder={'Confirm Password'}
+              className={'Confirm-Password'}
+            />
+          </div>
+
+
+        </div>
+        <div className='div_check'>
+          <input className='input_check' type="checkbox" name="my-checkbox" id="opt-in" />
+          <label className='label_check' for="opt-in">Acepto terminos y condiciones</label>
+        </div>
+
+
+        <div className='div_boton'>
+          <Button
+            className={'btn_login'}
+            text="REGISTRARSE"
+            color="var(--secondary-color)"
+            mode="fill"
+            textColor="var(--text-color-bg)"
+            onClick={() => {
+              handleClick('registrarse');
+            }}
+          />
+
+        </div>
+
+        <div>
+          <p className='P_text'>Already have an account? login</p>
+          
+        </div>
+
+
+
+
+      </div>
+
     </div>
+
+
+
+
   );
 }
 
